32656e54430a6ea9e02adcde2d8a5e12dfb07aa5
Joel Adams and Mariah Shipley <joel@>
2018-01-08 10:54:05 -0800

    add sub/multi/divide function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,25 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multi = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+
+
+alert("The sum is: " + add(number1, number2));
+alert("The difference is: " + sub(number1, number2));
+alert("The product is: " + multi(number1, number2));
+alert("The dividend is " + divide(number1, number2));
