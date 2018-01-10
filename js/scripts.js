27731d46966f7b53be3ef8679e9b49219bc45737
Andy Grossberg and Joel Adams <andy@>
2018-01-10 10:27:30 -0800

    add subtract, multiply, divide forms and front end jquery for each

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,27 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#addOutput").text(result);
+  });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = sub(number1, number2);
+    $("#subOutput").text(result);
+  });
+  $("form#multi").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multi1").val());
+    var number2 = parseInt($("#multi2").val());
+    var result = multi(number1, number2);
+    $("#multiOutput").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#divideOutput").text(result);
   });
 });
