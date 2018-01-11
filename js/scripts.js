4f0a1558b30b774ec705833710f4f6a8ef8b4105
Chris Swanner and Joel Adams <joel@>
2018-01-11 15:11:29 -0800

    add well to output text.

--- js/scripts.js
+++ js/scripts.js
@@ -18,32 +18,22 @@ var divide = function(number1, number2) {
 // Front End:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#addOutput").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = sub(number1, number2);
+    } else if (operator === "multiply") {
+      result = multi(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = sub(number1, number2);
-    $("#subOutput").text(result);
-  });
-  $("form#multi").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#multi1").val());
-    var number2 = parseInt($("#multi2").val());
-    var result = multi(number1, number2);
-    $("#multiOutput").text(result);
-  });
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-    $("#divideOutput").text(result);
-  });
+
 });
